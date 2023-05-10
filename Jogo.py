cbeb260cdc751526228102b2ef684070708ff55a
RaphaelSSaad <133152789+RaphaelSSaad@users.noreply.github.com>
2023-05-10 17:25:43 -0300

    Codigo 2(preenche frota)

--- Jogo.py
+++ Jogo.py
@@ -9,3 +9,20 @@ def define_posicoes(linha, coluna, orientacao, tamanho):
     return posicoes
 
 
+def preenche_frota(frota, navio, linha, coluna,orientacao, tamanho):
+    lista = []
+    if orientacao == 'vertical':
+        for i in range(0, tamanho):
+            lista.append([linha + i, coluna])
+
+    if orientacao == 'horizontal':
+        for i in range(0, tamanho):
+            lista.append([linha, coluna + i])
+
+    if navio in frota and lista != []:
+        frota[navio] += [lista]
+
+    if navio not in frota and lista != []:
+        frota[navio] = [lista]
+
+    return frota
