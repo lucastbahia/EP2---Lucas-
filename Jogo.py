5f0964d1125c602d9fa8692717cb87471ec49539
lucastbahia <133150057+lucastbahia@users.noreply.github.com>
2023-05-10 23:48:07 -0300

    Update Jogo.py - Código 8

--- Jogo.py
+++ Jogo.py
@@ -182,3 +182,107 @@ for embarcacao, qtde in dicionario_embarcacoes.items():
                 frota = preenche_frota(frota, embarcacao, linha, coluna, orientacao, qtde[1])
 
 print(frota)
+
+from random import randint
+
+frota = {
+    "porta-aviões":[],
+    "navio-tanque":[],
+    "contratorpedeiro":[],
+    "submarino": [],
+}
+
+dicionario_embarcacoes = {'porta-aviões': [1, 4], 'navio-tanque': [2, 3], 'contratorpedeiro': [3, 2], 'submarino': [4, 1]}
+
+for embarcacao, qtde in dicionario_embarcacoes.items():
+    
+    for i in range(0, qtde[0]):
+        print(f'Insira as informações referentes ao navio {embarcacao} que possui tamanho {qtde[1]}')
+
+        linha = int(input('Linha: '))
+        coluna = int(input('Coluna: '))
+
+        if embarcacao != 'submarino':
+            orientacao = int(input('[1] Vertical [2] Horizontal > '))
+
+            if orientacao == 1:
+                orientacao = 'vertical'
+            if orientacao == 2:
+                orientacao = 'horizontal'
+
+            if posicao_valida(frota, linha, coluna, orientacao, qtde[1]) == False:
+                while posicao_valida(frota, linha, coluna, orientacao, qtde[1]) == False:
+
+                    print('Esta posição não está válida!')
+                    print(f'Insira as informações referentes ao navio {embarcacao} que possui tamanho {qtde[1]}')
+
+                    linha = int(input('Linha: '))
+                    coluna = int(input('Coluna: '))
+
+                    if embarcacao != 'submarino':
+                        orientacao = int(input('[1] Vertical [2] Horizontal > '))
+
+                        if orientacao == 1:
+                            orientacao = 'vertical'
+                        if orientacao == 2:
+                            orientacao = 'horizontal'
+                
+                frota = preenche_frota(frota, embarcacao, linha, coluna, orientacao, qtde[1])
+            
+            else:
+                frota = preenche_frota(frota, embarcacao, linha, coluna, orientacao, qtde[1])
+
+        else:
+            orientacao = 'horizontal'
+
+            if posicao_valida(frota, linha, coluna, orientacao, qtde[1]) == False:
+                while posicao_valida(frota, linha, coluna, orientacao, qtde[1]) == False:
+
+                    print('Esta posição não está válida!')
+                    print(f'Insira as informações referentes ao navio {embarcacao} que possui tamanho {qtde[1]}')
+
+                    linha = int(input('Linha: '))
+                    coluna = int(input('Coluna: '))
+
+                    if embarcacao != 'submarino':
+                        orientacao = int(input('[1] Vertical [2] Horizontal > '))
+
+                        if orientacao == 1:
+                            orientacao = 'vertical'
+                        if orientacao == 2:
+                            orientacao = 'horizontal'
+
+                frota = preenche_frota(frota, embarcacao, linha, coluna, orientacao, qtde[1])
+                
+            else:
+                frota = preenche_frota(frota, embarcacao, linha, coluna, orientacao, qtde[1])
+
+print(frota)
+
+navios_afundados = 0
+
+tabuleiro = posiciona_frota(frota)
+
+lista_oponente = []
+
+linha_sorteada = randint(0, 9)
+coluna_sorteada = randint(0, 9)
+
+jogada = [linha_sorteada, coluna_sorteada]
+
+if jogada in lista_oponente:
+    while jogada in lista_oponente:
+        linha_sorteada = randint(0, 9)
+        coluna_sorteada = randint(0, 9)
+
+        jogada = [linha_sorteada, coluna_sorteada]
+
+else:
+    print(f'Seu oponente está atacando na linha {linha_sorteada} e coluna {coluna_sorteada}')
+
+tabuleiro = faz_jogada(tabuleiro, linha_sorteada, coluna_sorteada)
+
+navios_afundados = afundados(frota, tabuleiro)
+
+if navios_afundados == 10:
+    print('Xi! O oponente derrubou toda a sua frota =(')
